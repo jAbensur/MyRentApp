be581eadc853e2c2e56def973bec7aef8a04d82f
Rasel <ruselcucho@gmail.com>
2024-07-20 21:24:39 -0500

    Created interfaces for account management

--- app/src/main/java/com/myapplication/HomeActivity.java
+++ app/src/main/java/com/myapplication/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +14,47 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
+    TextView emailLoginTextView;
+    Button btnTenantManagement , btnAccountManagement;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initializeFields();
+        setupButtons();
+    }
+
+    private void initializeFields() {
+        emailLoginTextView = findViewById(R.id.Email_Login);
+
+        btnTenantManagement = findViewById(R.id.btnTenantManagement);
+        btnAccountManagement = findViewById(R.id.btnAccountManagement);
+    }
+
+    private void setupButtons() {
+        btnTenantManagement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToTenantManagement();
+            }
+        });
+
+        btnAccountManagement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToAccountManagement();
+            }
+        });
+    }
+
+    private void goToTenantManagement() {
+        Intent intent = new Intent(this, MainTenantActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToAccountManagement() {
+        Intent intent = new Intent(this, ManagementAccountActivity.class);
+        startActivity(intent);
     }
 }
