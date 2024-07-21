2bbe0a425f1523781ca9e472b45356e3811dd955
Rasel <ruselcucho@gmail.com>
2024-07-21 12:54:00 -0500

    Insert user defect

--- app/src/main/java/com/myapplication/database/Database.java
+++ app/src/main/java/com/myapplication/database/Database.java
@@ -2,8 +2,11 @@ package com.myapplication.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
 import com.myapplication.model.Chamber;
 import com.myapplication.model.Property;
 import com.myapplication.model.Rent;
@@ -16,9 +19,10 @@ import com.myapplication.repository.ChamberDao;
 import com.myapplication.repository.RentDao;
 
 @androidx.room.Database(entities = {User.class, Tenant.class, Chamber.class, Rent.class,
-        Property.class},exportSchema = false, version = 2)
+        Property.class}, exportSchema = false, version = 4)
 public abstract class Database extends RoomDatabase {
     private static Database instance;
+    private static Context appContext;
 
     public abstract UserDao userDao();
     public abstract TenantDao tenantDao();
@@ -26,11 +30,29 @@ public abstract class Database extends RoomDatabase {
     public abstract RentDao rentDao();
     public abstract PropertyDao propertyDao();
 
+    // insertar el usuario predeterminado al crear la base de datos
+    private static RoomDatabase.Callback roomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            // Run in a background thread
+            new Thread(() -> {
+                // Get the database instance
+                Database database = getInstance(appContext);
+                // Create a new user
+                User user = new User("Togata", "ruselcucho@gmail.com", "admin");
+                // Insert the user into the database
+                database.userDao().insert(user);
+            }).start();
+        }
+    };
+
     public static synchronized Database getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                             Database.class, "database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomDatabaseCallback)  // Add the callback here
                     .build();
         }
         return instance;
